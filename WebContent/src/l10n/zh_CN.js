da6a74fb68882e0b9507cfcb8a271e15aa043c85
老杜 <dyj095@126.com>
2018-10-12 11:39:31 +0800

    第一次提交

---
+++ WebContent/src/l10n/zh_CN.js
@@ -0,0 +1,72 @@
+{
+
+"header" : {
+  "navbar" : {
+    "UPLOAD" : "上传",
+    "new" : {
+      "NEW" : "待办",
+      "PROJECT" : "项目",
+      "TASK" : "任务",
+      "USER" : "用户",
+      "EMAIL" : "Email"
+    },
+    "NOTIFICATIONS" : "通知"
+  }
+},
+"aside" : {
+  "nav" : {
+    "HEADER" : "导航",
+    "DASHBOARD" : "首页",
+    "CALENDAR" : "日程",
+    "EMAIL" : "Email",
+    "WIDGETS" : "小部件",
+    "components" : {
+      "COMPONENTS" : "组件",
+      "ui_kits" : {
+        "UI_KITS" : "UI组件",
+        "BUTTONS" : "按钮",
+        "ICONS" : "图标",
+        "GRID" : "表格",
+        "BOOTSTRAP" : "Bootstrap",
+        "SORTABLE" : "可排序Table",
+        "PORTLET" : "门户",
+        "TIMELINE" : "时间轴",
+        "VECTOR_MAP" : "地图"
+      },
+      "table" : {
+        "TABLE" : "Table",
+        "TABLE_STATIC" : "Table static",
+        "DATATABLE" : "Datatable",
+        "FOOTABLE" : "Footable"
+      },
+      "form" : {
+        "FORM" : "表单",
+        "FORM_ELEMENTS" : "表单元素",
+        "FORM_VALIDATION" : "表单验证",
+        "FORM_WIZARD" : "表单向导"
+      },
+      "CHART" : "Chart",
+      "pages" : {
+        "PAGES" : "页面",
+        "PROFILE" : "个人资料",
+        "POST" : "发送",
+        "SEARCH" : "搜索",
+        "INVOICE" : "账单",
+        "LOCK_SCREEN" : "锁屏",
+        "SIGNIN" : "登录",
+        "SIGNUP" : "注册",
+        "FORGOT_PASSWORD" : "忘记密码",
+        "404" : "404"
+      }
+    },
+    "your_stuff" : {
+      "YOUR_STUFF": "个人空间",
+      "PROFILE" : "个人资料",
+      "DOCUMENTS" : "文档"
+    }
+  },
+  "MILESTONE" : "里程碑",
+  "RELEASE" : "发布"
+}
+
+}
